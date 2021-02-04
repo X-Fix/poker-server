246c1bf6b12437de41d5ee3ffa9428eaa62f1be6
Cameron Stroebel <cameron.stroebel08@gmail.com>
2021-02-05 00:20:40 +0100

    feat: Add ID utils

---
+++ src/utils/index.ts
@@ -0,0 +1,4 @@
+/* eslint-disable import/prefer-default-export */
+export { default as generateSessionId } from './generateSessionId';
+export { default as generateParticipantId } from './generateParticipantId';
+export * from './constants';
