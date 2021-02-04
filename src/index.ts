85dd053f7c3b2733ca63ef08751c2155bff42f0a
Cameron Stroebel <cameron.stroebel08@gmail.com>
2021-02-04 21:12:25 +0100

    feat: Add basic server communication

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,32 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import express from 'express';
+import { Socket } from 'socket.io';
+import compression from 'compression';
+import routes from './routes';
+import { Route } from './routes/Route';
 
 const app = express();
+app.set('port', process.env.PORT || 3000);
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => res.send({ status: 'ok' }));
+const http = require('http').Server(app);
+// set up socket.io and bind it to our
+// http server.
+const io = require('socket.io')(http);
 
-const server = app.listen(3000, () => {
+routes.forEach(({ handler, url }: Route) => app.get(url, handler));
+
+io.on('connection', (socket: Socket) => {
+  console.log('User connected', socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+const server = http.listen(3000, () => {
   console.log('Listening on port 3000');
 });
 
