5315b134e3d08e2b606ffdd00da19be05d7fd94d
Cameron Stroebel <cameron.stroebel08@gmail.com>
2021-02-05 20:36:52 +0100

    chore: disable no-console

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import server from './server';
 
 const port = process.env.PORT || 3000;
 const app = server.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`Listening on port: ${port}`);
 });
 
