ceb98f146b5cc293fd19ace327f92862d02303f4
Cameron Stroebel <cameron.stroebel08@gmail.com>
2021-02-05 22:36:31 +0100

    refactor: Move routes to single file in endpoints folder

--- src/routes/joinSession.ts
+++
@@ -1,9 +0,0 @@
-import { Route } from '../definitions';
-import { joinSession } from '../handlers';
-
-const joinSessionRoute: Route = {
-  handler: joinSession,
-  url: '/joinSession',
-};
-
-export default joinSessionRoute;
