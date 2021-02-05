fff6c38f9e8c60f5834250f774434a81adec9bbc
Cameron Stroebel <cameron.stroebel08@gmail.com>
2021-02-05 20:34:54 +0100

    chore: Organise tests into seperate files

---
+++ src/tests/server.spec.ts
@@ -0,0 +1,36 @@
+import supertest from 'supertest';
+import app from '../server';
+
+const server = app.listen(3001);
+const request = supertest(server);
+
+afterAll(() => {
+  app.close();
+});
+
+describe('/', () => {
+  it('should respond with JSON', async () => {
+    const { body, headers, status } = await request.get('/');
+
+    expect(status).toBe(200);
+    expect(headers['content-type']).toMatch(/json/);
+    expect(body.status).toEqual('ok');
+  });
+});
+
+describe('/ping', () => {
+  it('should respond to a ping', async () => {
+    const { status, text } = await request.get('/ping');
+
+    expect(status).toBe(200);
+    expect(text).toEqual('pong');
+  });
+});
+
+describe('unknown page', () => {
+  it('should respond with a 404', async () => {
+    const { status } = await request.get('/notFound');
+
+    expect(status).toBe(404);
+  });
+});
