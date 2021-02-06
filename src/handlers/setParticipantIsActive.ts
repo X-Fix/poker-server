c05ac869075bbad8d7963e6e7849f18d660e2752
Cameron Stroebel <cameron.stroebel08@gmail.com>
2021-02-06 20:07:52 +0100

    feat: Add set active participant socket message handler

---
+++ src/handlers/setParticipantIsActive.ts
@@ -0,0 +1,34 @@
+import { Namespace, Socket } from 'socket.io';
+import { SetParticipantIsActivePayload } from '../definitions';
+import { getSessionById } from '../stores/sessionStore';
+
+function setParticipantIsActive(
+  { isActive, participantId, sessionId }: SetParticipantIsActivePayload,
+  socket: Socket,
+  namespace: Namespace
+): void {
+  const session = getSessionById(sessionId);
+  const { ownerId, participants } = session;
+
+  // Find the acting participant by looking for a participant with matching socketId
+  // Prefer this over self-identifying methods like "I am the owner" (easier to fake)
+  const actingParticipant = participants.find(
+    ({ socketId }) => socketId === socket.conn.id
+  );
+
+  // Ignore if acting participant can't be found, or isn't the session owner
+  if (!actingParticipant || actingParticipant.id !== ownerId) return;
+
+  const targetParticipant = participants.find(({ id }) => id === participantId);
+
+  // Ignore if target participant can't be found
+  if (!targetParticipant) return;
+
+  // Set isActive to specified value
+  targetParticipant.isActive = isActive;
+
+  // Broadcast update to all subscribers of the socket group (room)
+  namespace.to(sessionId).emit('sync', session);
+}
+
+export default setParticipantIsActive;
