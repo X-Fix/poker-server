b7c89da8e241acfb92ac82febf28b935c7acdef1
Cameron Stroebel <cameron.stroebel08@gmail.com>
2021-02-06 12:01:24 +0100

    feat: Add dummySession endpoint for debug purposes

--- src/endpoints/index.ts
+++ src/endpoints/index.ts
@@ -1,10 +1,13 @@
+import { subscribe } from './messages';
 import {
   home,
   ping,
   createSessionRoute as createSession,
   joinSessionRoute as joinSession,
   resetRoute,
+  dummySessionRoute,
 } from './routes';
 
-export const get = [home, ping];
+export const get = [home, ping, dummySessionRoute];
 export const post = [createSession, joinSession, resetRoute];
+export const messages = [subscribe];
