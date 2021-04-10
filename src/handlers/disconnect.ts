390545a5b18075c21f6f68603550b57d4a7e838c
Cameron Stroebel <cameron.stroebel08@gmail.com>
2021-04-10 22:34:25 +0200

    fix: disconnect handler

--- src/handlers/disconnect.ts
+++ src/handlers/disconnect.ts
@@ -1,6 +1,6 @@
 import { Namespace, Socket } from 'socket.io';
 import { DisconnectPayload } from '../definitions';
-import { getSessionById } from '../stores/sessionStore';
+import { getSessionById, getSessionBySocketId } from '../stores/sessionStore';
 import { DISCONNECT_REASON } from '../utils';
 
 const { IO_SERVER_DISCONNECT, IO_CLIENT_DISCONNECT } = DISCONNECT_REASON;
@@ -16,36 +16,39 @@ function disconnect(
   socket: Socket,
   namespace: Namespace
 ): void {
-  console.log({ reason });
-  // If this disconnect was intentionally caused by the server or client, ignore it
-  if (isIntentionalDisconnect(reason)) return;
+  console.log({ reason }, socket.id);
+  const session = getSessionBySocketId(socket.id);
 
-  socket.rooms.forEach((sessionId) => {
-    const session = getSessionById(sessionId);
+  console.log('session found', Boolean(session));
+  // If no session found, assume this sessionId is actually the socketId, ignore and move on
+  if (!session) return;
 
-    // If no session found, assume this sessionId is actually the socketId, ignore and move on
-    if (!session) return;
+  const { participants } = session;
+  const disconnectedParticipant = participants.find(
+    ({ socketId }) => socketId === socket.id
+  );
 
-    const { participants } = session;
-    const disconnectedParticipant = participants.find(
-      ({ socketId }) => socketId === socket.id
-    );
+  console.log(
+    'disconnected participant found',
+    Boolean(disconnectedParticipant)
+  );
+  if (!disconnectedParticipant) return;
 
-    if (!disconnectedParticipant) return;
+  // Remove the participant's socketId and updated isConnected property
+  delete disconnectedParticipant.socketId;
+  disconnectedParticipant.isConnected = false;
 
-    // Remove the participant's socketId. This should signal to the rest that they are disconnected
-    delete disconnectedParticipant.socketId;
+  const connectedParticipants = participants.filter(({ socketId }) =>
+    Boolean(socketId)
+  );
+  if (!connectedParticipants.length) {
+    session.cleanUp = new Date(Date.now() + TEN_MINUTES);
+  }
+  console.log('session scheduled for cleanup', session.cleanUp?.toTimeString());
 
-    const connectedParticipants = participants.filter(({ socketId }) =>
-      Boolean(socketId)
-    );
-    if (!connectedParticipants.length) {
-      session.cleanUp = new Date(Date.now() + TEN_MINUTES);
-    }
-
-    // Broadcast update to all subscribers of the socket group (room)
-    namespace.to(sessionId).emit('syncParticipants', participants);
-  });
+  // Broadcast update to all subscribers of the socket group (room)
+  namespace.to(session.id).emit('syncParticipants', participants);
+  console.log('disconnect update broadcast');
 }
 
 export default disconnect;
--- src/stores/sessionStore.ts
+++ src/stores/sessionStore.ts
@@ -10,6 +10,23 @@ export function getSessionById(sessionId: string): Session {
   return sessionStore[sessionId];
 }
 
+export function getSessionBySocketId(socketId: string): Session | undefined {
+  let session: Session | undefined;
+
+  Object.keys(sessionStore).some((sessionId) => {
+    const hasParticipant = sessionStore[sessionId].participants?.some(
+      (participant) => socketId === participant.socketId
+    );
+    if (hasParticipant) {
+      session = sessionStore[sessionId];
+    }
+
+    return Boolean(session);
+  });
+
+  return session;
+}
+
 export function saveSession(session: Session): void {
   sessionStore[session.id] = session;
 }
