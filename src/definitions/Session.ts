03c4df7f232906dd328e875d5cc7f54f8c821006
Cameron Stroebel <cameron.stroebel08@gmail.com>
2021-05-24 14:05:34 +0200

    refactor: re-organise properties, make name required in constructor

--- src/definitions/Session.ts
+++ src/definitions/Session.ts
@@ -3,15 +3,15 @@ import Participant from './Participant';
 
 class Session {
   public id: string;
-  public name?: string;
+  public name: string;
   public ownerId: string;
   public participants: Participant[];
   public cardSequence: string[];
-  public topic?: string;
   public phase: 'lobby' | 'voting' | 'result'; // There is a 4th 'newTopic' phase but this is reserved for sessionOwners to privately set themselves to
+  public topic?: string;
   public cleanUp?: Date;
 
-  constructor(owner: Participant, cardSequence: string[], name?: string) {
+  constructor(owner: Participant, cardSequence: string[], name: string) {
     this.id = generateSessionId();
     this.name = name;
     this.ownerId = owner.id;
